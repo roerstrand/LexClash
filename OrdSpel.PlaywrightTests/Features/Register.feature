d13a913a9e5df233759b0aef2eebeb915416dcb2
ellenlinnea <linnea.schildt1@gmail.com>
2026-03-26 14:33:05 +0100

    Satt upp testprojekt: xUnit för API-projektet, nUnit + playwright

---
+++ OrdSpel.PlaywrightTests/Features/Register.feature
@@ -0,0 +1,15 @@
+﻿Feature: Register
+
+A short summary of the feature
+
+@tag1
+Scenario: [scenario name]
+	Given [context]
+	When [action]
+	Then [outcome]
+
+Scenario: Successful registration
+    Given I am on the register page
+    When I fill in username "testuser" and password "Test123!" and confirm the password
+    Then I should be redirected to the login page
+
---
+++ OrdSpel.PlaywrightTests/Hooks/Hooks.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Playwright;
+using TechTalk.SpecFlow;
+
+namespace OrdSpel.PlaywrightTests.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+        private IPlaywright _playwright;
+        private IBrowser _browser;
+        public IPage Page { get; private set; }
+
+        [BeforeScenario]
+        public async Task Setup()
+        {
+            _playwright = await Playwright.CreateAsync();
+            _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false // sätt till true för att köra utan webbläsare
+            });
+            Page = await _browser.NewPageAsync();
+        }
+
+        [AfterScenario]
+        public async Task Teardown()
+        {
+            await _browser.CloseAsync();
+            _playwright.Dispose();
+        }
+    }
+}
