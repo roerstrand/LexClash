4d37af3d603edb8e1ebdb9149c9130b3e220f50c
Filippa24 <filippa98b@hotmail.se>
2026-03-29 13:18:03 +0200

    controllers for category and word

---
+++ OrdSpel.API/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrdSpel.DAL.Data;
+
+namespace OrdSpel.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] //enpoint för att hämta categories
+        public async Task<IActionResult> GetCategories()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}/words")] //endpoint för att hämta orden i en specifik category
+        public async Task<IActionResult> GetCategoriesContent(int id)
+        {
+            var words = await _context.Words.Where(w => w.CategoryId == id).ToListAsync();
+
+            return Ok(words);
+        }
+    }
+}
---
+++ OrdSpel.API/Controllers/WordController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using OrdSpel.DAL.Data;
+
+namespace OrdSpel.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WordController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public WordController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] //enpoint för att hämta alla ord
+        public async Task<IActionResult> GetAllWords()
+        {
+            var words = await _context.Words.ToListAsync();
+            return Ok(words);
+        }
+        
+        //endpoint för att hämta ord i en specifik kategori finns i CategoryController
+
+    }
+}
