d7561839cae70b287d13319ed0b820f20ec37c78
MammaGula <supaphit.rue2@gmail.com>
2026-03-30 11:28:46 +0200

    deleted some comments

--- OrdSpel.DAL/Data/SeededData/SeededUserData.cs
+++ OrdSpel.DAL/Data/SeededData/SeededUserData.cs
@@ -24,43 +24,6 @@ namespace OrdSpel.DAL.Data.SeededData
                 await userManager.CreateAsync(user, "123");
             }
 
-
-
-
-            // Suggestion Version: A more robust seeding approach with error handling and support for multiple users.
-            //var users = new List<(string UserName, string Email)>
-            //{
-            //    ("spelare1", "spelare1@test.com"),
-            //    ("spelare2", "spelare2@test.com")
-            //};
-
-            //const string defaultPassword = "Test123!";
-
-            //foreach (var userData in users)
-            //{
-            //    var existingUser = await userManager.FindByNameAsync(userData.UserName);
-
-            //    if (existingUser != null)
-            //    {
-            //        continue;
-            //    }
-
-            //    var user = new IdentityUser
-            //    {
-            //        UserName = userData.UserName,
-            //        Email = userData.Email,
-            //        EmailConfirmed = true
-            //    };
-
-            //    var result = await userManager.CreateAsync(user, defaultPassword);
-
-            //    if (!result.Succeeded)
-            //    {
-            //        var errors = string.Join(", ", result.Errors.Select(e => e.Description));
-            //        throw new InvalidOperationException(
-            //            $"Failed to create seeded user '{userData.UserName}': {errors}");
-            //    }
-            //}
         }
     }
 }
