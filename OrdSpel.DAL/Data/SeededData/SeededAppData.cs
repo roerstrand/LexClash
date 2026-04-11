40db8dd0e635397fb4c756b3fa662bb53e899a9e
Filippa24 <filippa98b@hotmail.se>
2026-04-11 19:49:36 +0200

    restored previous seededappdata + added 2 missing countries

--- OrdSpel.DAL/Data/SeededData/SeededAppData.cs
+++ OrdSpel.DAL/Data/SeededData/SeededAppData.cs
@@ -39,93 +39,193 @@ namespace OrdSpel.DAL.Data.SeededData
 
             var countries = new List<string>
             {
-                // A
-"Afghanistan", "Albanien", "Algeriet", "Andorra", "Angola", "Antigua och Barbuda",
-"Argentina", "Armenien", "Australien", "Azerbajdzjan",
-
-// B
-"Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgien", "Belize",
-"Benin", "Bhutan", "Bolivia", "Bosnien och Hercegovina", "Botswana", "Brasilien",
-"Brunei", "Bulgarien", "Burkina Faso", "Burundi",
-
-// C
-"Centralafrikanska republiken", "Chile", "Colombia", "Comorerna",
-
-// D
-"Danmark", "Djibouti", "Dominica", "Dominikanska republiken",
-
-// E
-"Ecuador", "Egypten", "El Salvador", "Ekvatorialguinea", "Eritrea", "Estland",
-"Eswatini", "Etiopien",
-
-// F
-"Fiji", "Filippinerna", "Finland", "Frankrike",
-
-// G
-"Gabon", "Gambia", "Georgien", "Ghana", "Grekland", "Grenada", "Guatemala",
-"Guinea", "Guinea-Bissau", "Guyana",
-
-// H
-"Haiti", "Honduras",
-
-// I
-"Indien", "Indonesien", "Irak", "Iran", "Irland", "Island", "Israel", "Italien",
-
-// J
-"Jamaica", "Japan", "Jordanien",
-
-// K
-"Kambodja", "Kamerun", "Kanada", "Kap Verde", "Kazakstan", "Kenya", "Kina",
-"Kirgizistan", "Kiribati", "Komorerna", "Kongo-Brazzaville", "Kosovo", "Kroatien",
-"Kuba", "Kuwait",
-
-// L
-"Laos", "Lettland", "Libanon", "Liberia", "Libyen", "Liechtenstein", "Litauen",
+                "Afghanistan",
+"Albanien",
+"Algeriet",
+"Andorra",
+"Angola",
+"Antigua och Barbuda",
+"Argentina",
+"Armenien",
+"Australien",
+"Azerbajdzjan",
+"Bahamas",
+"Bahrain",
+"Bangladesh",
+"Barbados",
+"Belarus",
+"Vitryssland",
+"Belgien",
+"Belize",
+"Benin",
+"Bhutan",
+"Bolivia",
+"Bosnien och Hercegovina",
+"Botswana",
+"Brasilien",
+"Brunei",
+"Bulgarien",
+"Burkina Faso",
+"Burundi",
+"Centralafrikanska republiken",
+"Chile",
+"Colombia",
+"Comorerna",
+"Demokratiska republiken Kongo",
+"Danmark",
+"Djibouti",
+"Dominica",
+"Dominikanska republiken",
+"Ecuador",
+"Egypten",
+"El Salvador",
+"Ekvatorialguinea",
+"Eritrea",
+"Estland",
+"Eswatini",
+"Swaziland",
+"Etiopien",
+"Fiji",
+"Filippinerna",
+"Finland",
+"Frankrike",
+"Gabon",
+"Gambia",
+"Georgien",
+"Ghana",
+"Grekland",
+"Grenada",
+"Guatemala",
+"Guinea",
+"Guinea-Bissau",
+"Guyana",
+"Haiti",
+"Honduras",
+"Indien",
+"Indonesien",
+"Irak",
+"Iran",
+"Irland",
+"Island",
+"Israel",
+"Italien",
+"Jamaica",
+"Japan",
+"Jordanien",
+"Kambodja",
+"Kamerun",
+"Kanada",
+"Kap Verde",
+"Kazakstan",
+"Kenya",
+"Kina",
+"Kirgizistan",
+"Kiribati",
+"Kolombia",
+"Komorerna",
+"Kongo-Brazzaville",
+"Kosovo",
+"Kroatien",
+"Kuba",
+"Kuwait",
+"Laos",
+"Lettland",
+"Libanon",
+"Liberia",
+"Libyen",
+"Liechtenstein",
+"Litauen",
 "Luxemburg",
-
-// M
-"Madagaskar", "Malawi", "Malaysia", "Maldiverna", "Mali", "Malta", "Marocko",
-"Marshallöarna", "Mauretanien", "Mauritius", "Mexiko", "Mikronesien", "Moldavien",
-"Monaco", "Mongoliet", "Montenegro", "Mozambique", "Myanmar",
-
-// N
-"Namibia", "Nauru", "Nepal", "Nicaragua", "Nederländerna", "Niger", "Nigeria",
-"Nordkorea", "Nordmakedonien", "Norge", "Nya Zeeland",
-
-// O
+"Madagaskar",
+"Malawi",
+"Malaysia",
+"Maldiverna",
+"Mali",
+"Malta",
+"Marocko",
+"Marshallöarna",
+"Mauretanien",
+"Mauritius",
+"Mexiko",
+"Mikronesien",
+"Moldavien",
+"Monaco",
+"Mongoliet",
+"Montenegro",
+"Mozambique",
+"Myanmar",
+"Burma",
+"Namibia",
+"Nauru",
+"Nepal",
+"Nicaragua",
+"Nederländerna",
+"New Zealand",
+"Nya Zeeland",
+"Niger",
+"Nigeria",
+"Nordkorea",
+"Nordmakedonien",
+"Norge",
 "Oman",
-
-// P
-"Pakistan", "Palau", "Panama", "Papua Nya Guinea", "Paraguay", "Peru", "Polen",
+"Pakistan",
+"Palau",
+"Panama",
+"Papua Nya Guinea",
+"Paraguay",
+"Peru",
+"Polen",
 "Portugal",
-
-// Q
 "Qatar",
-
-// R
-"Republiken Kongo", "Rumänien", "Rwanda", "Ryssland",
-
-// S
-"Saint Kitts och Nevis", "Saint Lucia", "Saint Vincent och Grenadinerna", "Samoa",
-"San Marino", "Saudiarabien", "Senegal", "Serbien", "Seychellerna", "Sierra Leone",
-"Singapore", "Slovakien", "Slovenien", "Somalia", "Spanien", "Sri Lanka", "Sudan",
-"Surinam", "Sverige", "Schweiz", "Syrien", "Sydafrika", "Sydkorea", "Sydsudan",
-
-// T
-"Tadzjikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga",
-"Trinidad och Tobago", "Tunisien", "Turkiet", "Turkmenistan", "Tuvalu", "Taiwan", "Tyskland", "Tjeckien",
-
-// U
-"Uganda", "Ukraina", "Ungern", "Uruguay", "Uzbekistan",
-
-// V
-"Vanuatu", "Vatikanstaten", "Venezuela", "Vietnam",
-
-// Y
+"Republiken Kongo",
+"Rumänien",
+"Rwanda",
+"Ryssland",
+"Saint Kitts och Nevis",
+"Saint Lucia",
+"Saint Vincent och Grenadinerna",
+"Samoa",
+"San Marino",
+"Saudiarabien",
+"Senegal",
+"Serbien",
+"Seychellerna",
+"Sierra Leone",
+"Singapore",
+"Slovakien",
+"Slovenien",
+"Somalia",
+"Spanien",
+"Sri Lanka",
+"Sudan",
+"Surinam",
+"Sverige",
+"Schweiz",
+"Syrien",
+"Tadzjikistan",
+"Taiwan",
+"Tanzania",
+"Thailand",
+"Togo",
+"Tonga",
+"Trinidad och Tobago",
+"Tunisien",
+"Turkiet",
+"Turkmenistan",
+"Tuvalu",
+"Uganda",
+"Ukraina",
+"Ungern",
+"Uruguay",
+"Uzbekistan",
+"Vanuatu",
+"Vatikanstaten",
+"Venezuela",
+"Vietnam",
 "Yemen",
-
-// Z
-"Zambia", "Zimbabwe",
+"Jemen",
+"Zambia",
+"Zimbabwe"
             };
 
             foreach (var country in countries)
@@ -159,7 +259,7 @@ namespace OrdSpel.DAL.Data.SeededData
             {
                 // A
                 "abborre", "addax", "alpacka", "albatross", "antilop", "apa", "armadillo", "asp",
-                "anka", "alligator", "axolotl", "ara", "andalusisk häst", "and",
+                "anka", "alligator", "axolotl", "ara", "asna", "andalusisk häst", "and",
                 // B
                 "bagge", "bäver", "bi", "bläckfisk", "blomfluga", "bison", "björn", "blåmes", "brungås", "berguv",
                 "bisamråtta", "blåval", "buffel", "brunbjörn", "barracuda", "blåsfisk", "bergsget", "boafink",
@@ -210,7 +310,7 @@ namespace OrdSpel.DAL.Data.SeededData
                 "rådjur", "råtta", "ren", "ripa", "rocka", "rödhake", "regnbågsöring",
                 "räv", "rödräv", "rödspätta", "röding", "rapphöna", "rödstjärt",
                 // S
-                "säl", "sork", "sandödla", "skarv", "skata", "sköldpadda", "snok", "sengångare", "sjöhäst", "sjölejon",
+                "säl", "sandödla", "skarv", "skata", "sköldpadda", "snok", "sengångare", "sjöhäst", "sjölejon",
                 "svala", "spindel", "spindelkrabba", "struts", "stork", "svan", "surikat",
                 "sill", "snigel", "sparv", "stare", "stenbock", "sardin", "sjöstjärna", "skorpion", "salamander",
                 "silkesmask", "sidensvans", "sillgrissla", "svan",
